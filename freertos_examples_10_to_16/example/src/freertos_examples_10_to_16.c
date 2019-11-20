ff4c61dbde35b8fa3ba9385eba38cf88f29f2860
cristianlabo <cmg-88@hotmail.com>
2019-11-20 20:43:36 -0300

    TERCER commit

--- freertos_examples_10_to_16/example/src/freertos_examples_10_to_16.c
+++ freertos_examples_10_to_16/example/src/freertos_examples_10_to_16.c
@@ -49,11 +49,13 @@
 #define IMPLEMENTACION_1 (1) /* IMPLEMENTACION EJERCICIO 1*/
 #define IMPLEMENTACION_2 (2) /* IMPLEMENTACION EJERCICIO 2*/
 #define IMPLEMENTACION_3 (3) /* IMPLEMENTACION EJERCICIO 3*/
+#define MODULO_BLUETOOTH (4)
 
  //#define TEST (EXAMPLE_14)
 
-#define TEST IMPLEMENTACION_3
+//#define TEST IMPLEMENTACION_3
 
+#define TEST MODULO_BLUETOOTH
 
 
 /*****************************************************************************
@@ -1812,6 +1814,176 @@ int main(void)
     return ((int) NULL);
 }
 #endif
+
+
+#if (TEST == MODULO_BLUETOOTH)		/* Blocking when sending to a queue or sending structures on a queue */
+
+const char *pcTextForMain = "\r\nExample 10 - Blocking when sending to a queue or sending structures on a queue\r\n";
+
+#define mainSENDER_1		1
+#define mainSENDER_2		2
+
+/* The tasks to be created.  Two instances are created of the sender task while
+ * only a single instance is created of the receiver task. */
+static void vSenderTask(void *pvParameters);
+static void vReceiverTask(void *pvParameters);
+
+/* Declare a variable of type xQueueHandle.  This is used to store the queue
+ * that is accessed by all three tasks. */
+xQueueHandle xQueue;
+
+/* Define the structure type that will be passed on the queue. */
+typedef struct {
+	unsigned char ucValue;
+	unsigned char ucSource;
+} xData;
+
+/* Declare two variables of type xData that will be passed on the queue. */
+static const xData xStructsToSend[2] = {
+	{100, mainSENDER_1}, /* Used by Sender1. */
+	{200, mainSENDER_2}  /* Used by Sender2. */
+};
+
+
+/* Sender, UART (or output) & LED ON thread */
+static void vSenderTask(void *pvParameters)
+{
+	portBASE_TYPE xStatus;
+	const portTickType xTicksToWait = 100 / portTICK_RATE_MS;
+
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	while (1) {
+		Board_LED_Set(LED3, LED_ON);
+
+		/* The first parameter is the queue to which data is being sent.  The
+		 * queue was created before the scheduler was started, so before this task
+		 * started to execute.
+		 *
+		 * The second parameter is the address of the structure being sent.  The
+		 * address is passed in as the task parameter.
+		 *
+		 * The third parameter is the Block time - the time the task should be kept
+		 * in the Blocked state to wait for space to become available on the queue
+		 * should the queue already be full.  A block time is specified as the queue
+		 * will become full.  Items will only be removed from the queue when both
+		 * sending tasks are in the Blocked state.. */
+		xStatus = xQueueSendToBack(xQueue, pvParameters, xTicksToWait);
+
+		if (xStatus != pdPASS) {
+			/* We could not write to the queue because it was full - this must
+			 * be an error as the receiving task should make space in the queue
+			 * as soon as both sending tasks are in the Blocked state. */
+			DEBUGOUT("Could not send to the queue.\r\n");
+		}
+
+		/* Allow the other sender task to execute. */
+		taskYIELD();
+	}
+}
+
+/* Receiver, UART (or output) & LED OFF thread */
+static void vReceiverTask(void *pvParameters)
+{
+	/* Declare the structure that will hold the values received from the queue. */
+	xData xReceivedStructure;
+	portBASE_TYPE xStatus;
+
+	/* This task is also defined within an infinite loop. */
+	while (1) {
+		Board_LED_Set(LED3, LED_OFF);
+
+		/* As this task only runs when the sending tasks are in the Blocked state,
+		 * and the sending tasks only block when the queue is full, this task should
+		 * always find the queue to be full.  3 is the queue length. */
+		if (uxQueueMessagesWaiting(xQueue) != 3) {
+			DEBUGOUT("Queue should have been full!\r\n");
+		}
+
+		/* The first parameter is the queue from which data is to be received.  The
+		 * queue is created before the scheduler is started, and therefore before this
+		 * task runs for the first time.
+		 *
+		 * The second parameter is the buffer into which the received data will be
+		 * placed.  In this case the buffer is simply the address of a variable that
+		 * has the required size to hold the received structure.
+		 *
+		 * The last parameter is the block time - the maximum amount of time that the
+		 * task should remain in the Blocked state to wait for data to be available
+		 * should the queue already be empty.  A block time is not necessary as this
+		 * task will only run when the queue is full so data will always be available. */
+		xStatus = xQueueReceive(xQueue, &xReceivedStructure, (portBASE_TYPE)0);
+
+		if (xStatus == pdPASS) {
+			/* Data was successfully received from the queue, print out the received
+			 * value and the source of the value. */
+			if (xReceivedStructure.ucSource == mainSENDER_1) {
+				DEBUGOUT("From Sender 1 = %d\r\n", xReceivedStructure.ucValue);
+			}
+			else {
+				DEBUGOUT("From Sender 2 = %d\r\n", xReceivedStructure.ucValue);
+			}
+		}
+		else {
+			/* We did not receive anything from the queue.  This must be an error
+			 * as this task should only run when the queue is full. */
+			DEBUGOUT("Could not receive from the queue.\r\n");
+		}
+	}
+}
+
+
+/*****************************************************************************
+ * Public functions
+ ****************************************************************************/
+/**
+ * @brief	main routine for FreeRTOS example 11 - Blocking when sending to a queue or sending structures on a queue
+ * @return	Nothing, function should not exit
+ */
+int main(void)
+{
+	/* Sets up system hardware */
+	prvSetupHardware();
+
+	/* Print out the name of this example. */
+	DEBUGOUT(pcTextForMain);
+
+    /* The queue is created to hold a maximum of 3 structures of type xData. */
+    xQueue = xQueueCreate(3, sizeof(xData));
+
+	if (xQueue != (xQueueHandle)NULL) {
+		/* Create two instances of the task that will write to the queue.  The
+		 * parameter is used to pass the structure that the task should write to the
+		 * queue, so one task will continuously send xStructsToSend[ 0 ] to the queue
+		 * while the other task will continuously send xStructsToSend[ 1 ].  Both
+		 * tasks are created at priority 2 which is above the priority of the receiver. */
+		xTaskCreate(vSenderTask, (char *) "vSender1", configMINIMAL_STACK_SIZE, (void *) &(xStructsToSend[0]),
+					(tskIDLE_PRIORITY + 2UL), (xTaskHandle *) NULL);
+		xTaskCreate(vSenderTask, (char *) "vSender2", configMINIMAL_STACK_SIZE, (void *) &(xStructsToSend[1]),
+					(tskIDLE_PRIORITY + 2UL), (xTaskHandle *) NULL);
+
+		/* Create the task that will read from the queue.  The task is created with
+		 * priority 1, so below the priority of the sender tasks. */
+		xTaskCreate(vReceiverTask, (char *) "vReceiver", configMINIMAL_STACK_SIZE, NULL,
+					(tskIDLE_PRIORITY + 1UL), (xTaskHandle *) NULL);
+
+	/* Start the scheduler */
+	vTaskStartScheduler();
+	}
+	else {
+		/* The queue could not be created. */
+	}
+
+    /* If all is well we will never reach here as the scheduler will now be
+     * running the tasks.  If we do reach here then it is likely that there was
+     * insufficient heap memory available for a resource to be created. */
+	while (1);
+
+	/* Should never arrive here */
+	return ((int) NULL);
+}
+#endif
+
+
 
 
 /**
--- lpc_chip_43xx/Debug/src/ring_buffer.d
+++ lpc_chip_43xx/Debug/src/ring_buffer.d
@@ -1,7 +1,7 @@
 src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- C:\Users\cmg-8\Documents\LPCXpresso_8.2.0_647\workspace-TP3\lpc_chip_43xx\inc/ring_buffer.h \
- C:\Users\cmg-8\Documents\LPCXpresso_8.2.0_647\workspace-TP3\lpc_chip_43xx\inc/lpc_types.h
+ C:\Users\cmg-8\Documents\LPCXpresso_8.2.0_647\workspace-modulo-bluetooth\modulobluetooth\lpc_chip_43xx\inc/ring_buffer.h \
+ C:\Users\cmg-8\Documents\LPCXpresso_8.2.0_647\workspace-modulo-bluetooth\modulobluetooth\lpc_chip_43xx\inc/lpc_types.h
 
-C:\Users\cmg-8\Documents\LPCXpresso_8.2.0_647\workspace-TP3\lpc_chip_43xx\inc/ring_buffer.h:
+C:\Users\cmg-8\Documents\LPCXpresso_8.2.0_647\workspace-modulo-bluetooth\modulobluetooth\lpc_chip_43xx\inc/ring_buffer.h:
 
-C:\Users\cmg-8\Documents\LPCXpresso_8.2.0_647\workspace-TP3\lpc_chip_43xx\inc/lpc_types.h:
+C:\Users\cmg-8\Documents\LPCXpresso_8.2.0_647\workspace-modulo-bluetooth\modulobluetooth\lpc_chip_43xx\inc/lpc_types.h:
